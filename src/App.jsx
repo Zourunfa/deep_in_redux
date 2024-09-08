1f0f3e981502123d1ab9f3c7b818b125033f41c3
Zourunfa <473023989@qq.com>
2024-09-08 13:00:16 +0800

    feat: connect

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export const App = () => {
   )
 }
 const 大儿子 = () => <section>大儿子<User/></section>
-const 二儿子 = () => <section>二儿子<Wrapper/></section>
+const 二儿子 = () => <section>二儿子<UserModifier x={'123'}>你好吖</UserModifier></section>
 const 幺儿子 = () => <section>幺儿子</section>
 const User = () => {
   const contextValue = useContext(appContext)
@@ -50,7 +50,43 @@ const  Wrapper = ()=>{
 
   return <UserModifier dispatch={dispatch} state={appState}/>
 }
-const UserModifier = ({dispatch,state}) => {
+
+// userModifier组件 通过上面的wrapper的封装能够读写全局的state和使用dispatch
+// 那么我们肯定会有很多组件，每个组件都这样写一遍肯定是成本很高的
+// 这时候我们就需要抽离一个高阶函数组件完成这件事
+const connect = (Component) =>{
+  return (props)=>{
+    const {appState, setAppState} = useContext(appContext)
+
+    const dispatch = (action) => {
+      setAppState(reducer(appState,action))
+    }
+  
+    return <Component {...props} dispatch={dispatch} state={appState}/>
+  }
+}
+
+
+// const _UserModifier = ({dispatch,state}) => {
+
+
+//   const onChange = (e) => {
+  
+
+//     // setAppState 会触发 contextValue.appState 的更新，前提条件是不能给原来对象的引用，必须给一个新的对象
+//     // 此原因从而引导reducer的诞生{...contextValue.appState}
+//     // setAppState(reducer(appState,{type:'updateUser',payload:{name:e.target.value}}))
+
+//     dispatch({type:'updateUser',payload:{name:e.target.value}})
+//   }
+//   return <div>
+//     <input value={state.user.name}
+//       onChange={onChange}/>
+//   </div>
+// }
+
+
+const UserModifier = connect(({dispatch,state,children}) => {
 
 
   const onChange = (e) => {
@@ -63,8 +99,9 @@ const UserModifier = ({dispatch,state}) => {
     dispatch({type:'updateUser',payload:{name:e.target.value}})
   }
   return <div>
+    {children}
     <input value={state.user.name}
       onChange={onChange}/>
   </div>
 }
-
+)
