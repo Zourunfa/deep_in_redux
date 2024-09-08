6ee7b389fcdbb4c40ae0bd524a5e2cead68a981d
Zourunfa <473023989@qq.com>
2024-09-08 12:24:42 +0800

    feat: reducer

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export const App = () => {
   )
 }
 const 大儿子 = () => <section>大儿子<User/></section>
-const 二儿子 = () => <section>二儿子<UserModifier/></section>
+const 二儿子 = () => <section>二儿子<Wrapper/></section>
 const 幺儿子 = () => <section>幺儿子</section>
 const User = () => {
   const contextValue = useContext(appContext)
@@ -24,7 +24,7 @@ const User = () => {
 
 }
 
-// reducer
+// reducer 规范state的创建流程
 const reducer = (state,{type,payload})=>{
   if(type === 'updateUser'){
     return {
@@ -38,18 +38,32 @@ const reducer = (state,{type,payload})=>{
     return state
   }
 }
-const UserModifier = () => {
+  // react规定只能在组件内部使用Hooks,
+  // 为了使得dispatch函数被抽离，dispatch的使用需要通过一个外层组件包裹
+  // dispatch 的目的是为了规范setState的流程
+const  Wrapper = ()=>{
   const {appState, setAppState} = useContext(appContext)
 
+  const dispatch = (action) => {
+    setAppState(reducer(appState,action))
+  }
+
+  return <UserModifier dispatch={dispatch} state={appState}/>
+}
+const UserModifier = ({dispatch,state}) => {
+
+
   const onChange = (e) => {
-    appState.user.name = e.target.value
+  
 
     // setAppState 会触发 contextValue.appState 的更新，前提条件是不能给原来对象的引用，必须给一个新的对象
     // 此原因从而引导reducer的诞生{...contextValue.appState}
-    setAppState(reducer(appState,{type:'updateUser',payload:{name:e.target.value}}))
+    // setAppState(reducer(appState,{type:'updateUser',payload:{name:e.target.value}}))
+
+    dispatch({type:'updateUser',payload:{name:e.target.value}})
   }
   return <div>
-    <input value={appState.user.name}
+    <input value={state.user.name}
       onChange={onChange}/>
   </div>
 }
